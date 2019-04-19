119ef628ca62de80d59c4230ef87eb51d9a589f9
drew-loukusa <dlwerd@gmail.com>
2019-04-19 16:18:44 -0700

    For you Jayden owo.

--- test_server.py
+++ test_server.py
@@ -20,14 +20,14 @@ def allowed_file(filename):
 @application.route('/', methods=['GET', 'POST'])
 def upload_file():		
 	if request.method == 'GET':
-		return render_template("test_html.html")
+		return render_template("sneakserv.html")
 
 	if request.method == 'POST':
 		print("Uplading a file..")
 		# check if the post request has the file part
 		if 'file' not in request.files:
 			flash("No file was selected for upload.")
-			return render_template("test_html.html")
+			return render_template("sneakserv.html")
 		
 		file = request.files['file']
 
@@ -35,18 +35,18 @@ def upload_file():
 		# submit an empty part without filename
 		if file.filename == '':
 			flash("File name is blank.")		
-			return redirect("test_html.html")
+			return redirect("sneakserv.html")
 
 		if file and allowed_file(file.filename):
 			flash("Uploading: ", file.filename)
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
 			#return redirect(url_for('uploaded_file',filename=filename))
-			return render_template("test_html.html")
+			return render_template("sneakserv.html")
 		else:
 			flash("File type not supported!")
 
-	return render_template("test_html.html")
+	return render_template("sneakserv.html")
 
 	
 
