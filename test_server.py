bf1fed51dc276dc2a3378d683c17cd45bf37d2f8
drew-loukusa <dlwerd@gmail.com>
2019-04-19 15:50:34 -0700

    OWO OWO OWO

--- test_server.py
+++ test_server.py
@@ -6,47 +6,47 @@ from flask import Flask, request, flash, redirect, render_template, session, url
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = os.getcwd()+"/UPLOAD_FOLDER"
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['zip'])
 
 application = Flask(__name__)
 application.secret_key = 'some_secret'
 application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 	
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @application.route('/', methods=['GET', 'POST'])
 def upload_file():		
 	if request.method == 'GET':
 		return render_template("test_html.html")
 
-
 	if request.method == 'POST':
-		print("GOT HERE")
+		print("Uplading a file..")
 		# check if the post request has the file part
 		if 'file' not in request.files:
-			flash('No file part')
-			print("No file part")
-			return redirect(request.url)
+			flash("No file was selected for upload.")
+			return render_template("test_html.html")
+		
 		file = request.files['file']
+
 		# if user does not select file, browser also
 		# submit an empty part without filename
 		if file.filename == '':
-			print("No file selected.s")
-			#flash('No selected file')
-			return redirect(request.url)
-		if file:
-			print("YO WADDUP")
+			flash("File name is blank.")		
+			return redirect("test_html.html")
+
+		if file and allowed_file(file.filename):
+			flash("Uploading: ", file.filename)
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
 			#return redirect(url_for('uploaded_file',filename=filename))
 			return render_template("test_html.html")
-	return '''
-	<!doctype html>
-	<title>Upload new File</title>
-	<h1>Upload new File</h1>
-	<form method=post enctype=multipart/form-data>
-	  <input type=file name=file>
-	  <input type=submit value=Upload>
-	</form>
-	'''
+		else:
+			flash("File type not supported!")
+
+	return render_template("test_html.html")
 
 	
 
